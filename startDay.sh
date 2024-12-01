da09b54461b1ab71dc16b38cbf020788cbf1a534
muffie <mufasachan@hotmail.com>
2024-12-01 11:38:42 +0100

    utils start

---
+++ startDay.sh
@@ -0,0 +1,10 @@
+# credit https://github.com/Janiczek/advent-of-code/blob/master/start.sh#L8-L11
+day=$1
+dayNoZero=${day#0}
+
+# COOKIE_SESSION is defined
+source .env
+
+mkdir $day && cd $day
+curl "https://adventofcode.com/2024/day/$dayNoZero/input" -H "cookie: session=$COOKIE_SESSION" -o input.txt
+nvim p1.sh p2.sh input.txt test.txt
